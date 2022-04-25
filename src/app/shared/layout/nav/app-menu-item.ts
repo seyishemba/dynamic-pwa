05c37c3d0f8c4b15d5c4094054ba3c29f423f846
Osman <Osman@DESKTOP-67UFDTK>
2022-04-25 13:23:49 +0000

    Initial commit

---
+++ src/app/shared/layout/nav/app-menu-item.ts
@@ -0,0 +1,58 @@
+﻿export class AppMenuItem {
+    name = '';
+    permissionName = '';
+    icon = '';
+    route = '';
+    routeErps = [];
+    items: AppMenuItem[];
+    external: boolean;
+    requiresAuthentication: boolean;
+    featureDependency: any;
+    parameters: {};
+
+    constructor(
+        name: string,
+        permissionName: string,
+        icon: string,
+        route: string,
+        routeErps?: string[],
+        items?: AppMenuItem[],
+        external?: boolean,
+        parameters?: Object,
+        featureDependency?: any,
+        requiresAuthentication?: boolean
+    ) {
+        this.name = name;
+        this.permissionName = permissionName;
+        this.icon = icon;
+        this.route = route;
+        this.routeErps = routeErps;
+        this.external = external;
+        this.parameters = parameters;
+        this.featureDependency = featureDependency;
+
+        if (items === undefined) {
+            this.items = [];
+        } else {
+            this.items = items;
+        }
+
+        if (this.permissionName) {
+            this.requiresAuthentication = true;
+        } else {
+            this.requiresAuthentication = requiresAuthentication ? requiresAuthentication : false;
+        }
+    }
+
+    hasFeatureDependency(): boolean {
+        return this.featureDependency !== undefined;
+    }
+
+    featureDependencySatisfied(): boolean {
+        if (this.featureDependency) {
+            return this.featureDependency();
+        }
+
+        return false;
+    }
+}
